c5303c54cf7db46faccd47c44be4034d303bfd78
phoebewalquist <pheebijane@yahoo.com>
2023-05-04 20:43:30 -0700

    refractor skills skills to use partials

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Skills' });
+  res.render('index', { title: 'Orange-in' });
 });
 
 module.exports = router;
